67a94b790a333467aa2b32e8b81e28193daf21ee
Thoronir42 <Thoronir42@Gmail.com>
2016-12-23 21:19:39 +0100

    Implemented url aliasing prototype

--- extensions/UrlAlias/UrlAliasExtension.php
+++ extensions/UrlAlias/UrlAliasExtension.php
@@ -3,14 +3,31 @@
 namespace KiwiBladeExtensions\UrlAlias;
 
 
-use KiwiBlade\DI\AConfiguratorExtension;
+use KiwiBlade\DI\AContainerExtension;
 use KiwiBlade\DI\Configurator;
 use KiwiBlade\DI\Container;
+use KiwiBlade\Http\Request;
+use KiwiBlade\Http\RequestFactory;
 
-class UrlAliasExtension extends AConfiguratorExtension
+class UrlAliasExtension extends AContainerExtension
 {
-    public function registerServices(Configurator $configurator, Container $container)
+    public function registerServices(Container $container)
     {
         $container->autoregisterService(UrlAliaser::class, 'urlAliaser');
+
+        /** @var UrlAliaser $aliaser */
+        $aliaser = $container->get(UrlAliaser::class);
+        /** @var Request $request */
+        $request = $container->get(Request::class);
+        $query = $request->getParam(RequestFactory::NICE_URL_QUERY_FIELD);
+
+        if ($aliaser->aliasExists($query)) {
+            $parts = $aliaser->decodeAlias($query);
+        } else {
+            $parts = $aliaser->decodeParts($request->getController(), $request->getAction());
+        }
+
+        $request->setController($parts['controller']);
+        $request->setAction($parts['action']);
     }
 }
--- extensions/UrlAlias/UrlAliaser.php
+++ extensions/UrlAlias/UrlAliaser.php
@@ -5,8 +5,109 @@ namespace KiwiBladeExtensions\UrlAlias;
 
 class UrlAliaser
 {
-    public function __construct($aliases)
+    private $aliases;
+    private $aliasMap;
+
+    private $controllers;
+    private $localisedControllers;
+
+    public function __construct($aliases, $controllers = [])
     {
-        dump($aliases);exit;
+        $this->setAliases($aliases);
+        $this->setControllers($controllers);
+    }
+
+    public function encode($controller, $action)
+    {
+        if (array_key_exists($controller, $this->aliasMap)) {
+            if (array_key_exists($action, $this->aliasMap[$controller])) {
+                return $this->aliasMap[$controller][$action];
+            }
+        }
+
+
+        return null;
+    }
+
+    public function aliasExists($alias)
+    {
+        return array_key_exists($alias, $this->aliases);
+    }
+
+    public function decodeAlias($alias)
+    {
+        if ($this->aliasExists($alias)) {
+            return $this->aliases[$alias];
+        }
+
+        return null;
+    }
+
+    public function decodeParts($controller, $action){
+        if(!isset($this->localisedControllers[$controller])){
+            throw new \InvalidArgumentException("Localised controller $controller does not exist");
+        }
+        if(!isset($this->localisedControllers[$controller]['actions'][$action])){
+            throw new \InvalidArgumentException("Localised action $controller:$action does not exist");
+        }
+        return [
+            'controller' => $this->localisedControllers[$controller]['name'],
+            'action' => $this->localisedControllers[$controller]['actions'][$action],
+        ];
+    }
+
+    private function setAliases($aliases)
+    {
+        $this->aliases = [];
+        $this->aliasMap = [];
+        foreach ($aliases as $name => $alias) {
+            if (!is_array($alias) || sizeof($alias) != 2) {
+                throw new \InvalidArgumentException("Alias $name is not valid");
+            }
+            list($controller, $action) = $alias;
+
+            if (isset($this->aliasMap[$controller][$name])) {
+                throw new \InvalidArgumentException("Duplicate alias for $controller:$name");
+            }
+            $this->aliases[$name] = [
+                'controller' => $controller,
+                'action' => $action,
+            ];
+            $this->aliasMap[$controller][$action] = $name;
+        }
+    }
+
+    private function setControllers($controllers)
+    {
+        $this->controllers = [];
+        $this->localisedControllers = [];
+
+        foreach ($controllers as $controller => $definition) {
+            if (!is_array($definition) || !array_key_exists('name', $definition) ||
+                !array_key_exists('actions', $definition) || !is_array($definition['actions'])
+            ) {
+                throw new \InvalidArgumentException("Controller alias $controller is not valid");
+            }
+            $this->putController($controller, $definition['name'], $definition['actions']);
+        }
+    }
+
+    private function putController($name, $localisedName, $actions)
+    {
+        if (isset($this->localisedControllers[$localisedName])) {
+            throw new \InvalidArgumentException("Localised controller name $localisedName is already defined");
+        }
+
+        $this->controllers[$name] = ['name' => $localisedName];
+        $this->localisedControllers[$localisedName] = ['name' => $name];
+
+
+        foreach ($actions as $action => $localisedAction) {
+            if (isset($this->localisedControllers[$localisedName][$localisedAction])) {
+                throw new \InvalidArgumentException("Localised controller action $localisedName:$localisedAction is already defined");
+            }
+            $this->controllers[$name]['actions'][$action] = $localisedAction;
+            $this->localisedControllers[$localisedName]['actions'][$localisedAction] = $action;
+        }
     }
 }
